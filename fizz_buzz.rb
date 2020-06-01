735605b709f6a7858e703461377d44b3560fea4d
masahira364 <seka-sote-kc.san2530te@ezweb.ne.jp>
2020-06-01 21:32:33 +0900

    [start]2-9

---
+++ fizz_buzz.rb
@@ -0,0 +1,19 @@
+def fizz_buzz(number)
+	if (number) % 3 == 0
+		puts "Fizz"
+	elsif (number) % 5 == 0
+		puts "Buzz"
+	elsif (number) % 15 ==0
+		puts "FizzBuzz"
+	else
+		(number).to_s
+	end
+end
+
+	puts "1 以上の数字を入力してください"
+
+	input = gets.to_i
+
+	puts "結果は、、、"
+
+	puts fizz_buzz(input)
